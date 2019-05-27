b32b21167b51e5c560eeb74bc1203e1910e45ca4
Slava Kurganov <sgorelchik@mail.ru>
2019-05-27 17:41:06 +0300

    обнавлен alphabet
    Добавлен алфавит цифр

--- RSACriptografic/Classes/Alphabet.cs
+++ RSACriptografic/Classes/Alphabet.cs
@@ -11,5 +11,6 @@ namespace ElGamalCriptografic.Classes
         public static readonly string alphabetRuLow = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
         public static readonly string alphabetRuUP = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
         public static readonly string alphabetEnUP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        public static readonly string alphabetNumber = "1234567890";
     }
 }
